2783a1722a947a3acd65c9f085f39b6954b41024
Guillaume Blaquiere <guillaume.blaquiere@veolia.com>
2020-03-20 15:01:58 +0100

    Update with external project capability and multiligne secret

--- Dockerfile
+++ Dockerfile
@@ -15,7 +15,10 @@ RUN CGO_ENABLED=0 GOOS=linux go build -v -o server
 # Gcloud capable image
 FROM google/cloud-sdk
 
+# Install additional runtime is required
+# Python 3.7.3 and Java OpenJDK 1.8.0_242 already installed (March 2020 version)
+
 COPY --from=builder /app/server /server
-COPY --from=builder /app/start-gcloud.sh /start.sh
+COPY --from=builder /app/start.sh /start.sh
 RUN chmod +x /start.sh
 CMD ["/start.sh", "/server"]
--- start.sh
+++ start.sh
@@ -7,22 +7,38 @@ secret_prefix="secret:"
 # load the secrets
 for i in $(printenv | grep ${secret_prefix})
 do
-        key=$(echo ${i} | cut -d'=' -f 1)
-        val=$(echo ${i} | cut -d'=' -f 2-)
-       if [[ ${val} == ${secret_prefix}* ]]
-       then
-               val=$(echo ${val} | sed -e "s/${secret_prefix}//g")
-               echo $val
-               secret=$(echo ${val} | cut -d'#' -f 1)
-               version=$(echo ${val} | cut -d'#' -f 2)
-               echo $secret
-               if [ -z ${version} ]
-               then
-                       version="latest"
-               fi
-               echo $version
-               export $key="$(gcloud beta secrets versions access --secret=${secret} ${version})"
-       fi
+  key=$(echo ${i} | cut -d'=' -f 1)
+  val=$(echo ${i} | cut -d'=' -f 2-)
+  if [[ ${val} == ${secret_prefix}* ]]
+  then
+    val=$(echo ${val} | sed -e "s/${secret_prefix}//g")
+    # Pattern of a secret projectId/secretName#version
+    # projectId is optional but / required
+    # version and # are optional
+    # Example of valid secret
+    # /my_secret
+    # /my_secret#1
+    # /my_secret#latest
+    # projectID/my_secret
+    # projectID/my_secret#2
+    projectId=$(echo ${val} | cut -d'/' -f 1)
+    secret=$(echo ${val} | cut -d'/' -f 2)
+
+    if [[ -n ${projectId} ]]
+    then
+      project="--project=${projectId}"
+    fi
+
+    secretName=$(echo ${secret} | cut -d'#' -f 1)
+    version="latest"
+    if [[ ${val} == *#* ]]
+    then
+      version=$(echo ${val} | cut -d'#' -f 2)
+    fi
+    plain="$(gcloud beta secrets versions access --secret=${secretName} ${version} ${project})"
+    #For multiline management
+    export $key="$(echo $plain | sed -e 's/\n//g')"
+  fi
 done
 
 #run the following command
